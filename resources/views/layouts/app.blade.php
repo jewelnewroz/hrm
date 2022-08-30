26507160ad37249cc6b968f1bb3f50e83dec6fa1
Jewel Rana <jewel@newroztech.com>
2022-08-30 18:56:16 +0600

    Menu setting done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Dashboard 2</title>
+    <title>{{ config('app.name') }} | {{ $title ?? '' }}</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -13,6 +13,7 @@
     <link rel="stylesheet" href="{{ asset('adminLte/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
     <!-- Theme style -->
     <link rel="stylesheet" href="{{ asset('adminLte/dist/css/adminlte.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
 </head>
 <body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
 <div class="wrapper">
@@ -60,10 +61,10 @@
 
     <!-- Main Footer -->
     <footer class="main-footer">
-        <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+        <strong>Copyright &copy; {{ date('Y') }} <a href="#">{{ config('app.name') }}</a>.</strong>
         All rights reserved.
         <div class="float-right d-none d-sm-inline-block">
-            <b>Version</b> 3.2.0
+            <b>Version</b> 1.1.0
         </div>
     </footer>
 </div>
--- resources/views/layouts/elements/sidebar.blade.php
+++ resources/views/layouts/elements/sidebar.blade.php
@@ -2,9 +2,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ route('dashboard') }}" class="brand-link">
-        <img src="{{ asset('adminLte/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
-             class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">{{ config('') }}</span>
+        <img src="{{ asset('assets/img/logo-demo.png') }}" title="{{ config('') }}" class="brand-image" style="opacity: .8">
     </a>
 
     <!-- Sidebar -->
@@ -12,70 +10,44 @@
         <!-- Sidebar Menu -->
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                <li class="nav-item  menu-open">
-                    <a href="{{ route('dashboard') }}">
-                        <i class="fa fa-home"></i> Dashboard </a>
+                <li class="nav-item">
+                    <a href="{{ route('dashboard') }}" class="nav-link active">
+                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <p>Dashboard <i class="right fas fa-angle-left"></i></p>
+                    </a>
                 </li>
-
-
-                    @roleorpermission(['admin', 'employee-list'])
-                    <li class="nav-item @if(in_array(request()->segment(2), ['employee', 'designation', 'department'] ) ) menu-open @endif">
-                        <a href="">
-                            <i class="fa fa-user-secret"></i> Employees <i class="fa arrow"></i>
-                        </a>
-                        <ul class="sidebar-nav @if(in_array(request()->segment(2), ['employee', 'designation', 'department'])) collapse in @endif">
-                            @can('customer-create')
-                                <li class="@if(request()->segment(2) == 'employee' && request()->segment(3) == 'create' ) active @endif">
-                                    <a href="{{ route('employee.create') }}"><i class="fa fa-plus"></i> Add
-                                        new employee</a>
-                                </li>
-                            @endcan
-                            <li class="@if(request()->segment(2) == 'employee' && request()->segment(3) == '' ) active @endif">
-                                <a href="{{ route('employee.index') }}"><i class="fa fa-user"></i>
-                                    Employees</a>
-                            </li>
-                            @can('department-list')
-                                <li class="@if(request()->segment(2) == 'department' && request()->segment(3) == '' ) active @endif">
-                                    <a href="{{ route('department.index') }}"><i
-                                            class="fa fa-code-branch"></i> Departments</a>
-                                </li>
-                            @endcan
-                            @can('designation-list')
-                                <li class="@if(request()->segment(2) == 'designation' && request()->segment(3) == '' ) active @endif">
-                                    <a href="{{ route('designation.index') }}"><i class="fa fa-tag"></i>
-                                        Designations</a>
-                                </li>
-                            @endcan
-                        </ul>
-                    </li>
-                    @endroleorpermission
-                    @hasanyrole('admin')
-                    <li class="nav-item @if(in_array(request()->segment(2), ['user', 'role', 'permission', 'option'] ) ) menu-open @endif">
-                        <a href="">
-                            <i class="fa fa-cogs"></i> Manage <i class="fa arrow"></i>
-                        </a>
-                        <ul class="sidebar-nav  @if(in_array(request()->segment(2), ['user', 'role', 'permission'] ) ) collapse in @endif">
-                            <li class="@if(request()->segment(2) == 'currency' && request()->segment(3) == '' ) active @endif">
-                                <a href="{{ route('currency.index')}}"><i class="fa fa-usd"></i>
-                                    Currencies </a>
-                            </li>
-                            <li class="@if(request()->segment(2) == 'user' && request()->segment(3) == '' ) active @endif">
-                                <a href="{{ route('user.index')}}"><i class="fa fa-user-secret"></i>
-                                    Administrators </a>
-                            </li>
-                            <li class="@if(request()->segment(2) == 'role' && request()->segment(3) == '' ) active @endif">
-                                <a href="{{ route('role.index') }}"><i class="fa fa-tag"></i> Roles </a>
-                            </li>
-                            <li class="@if(request()->segment(2) == 'permission' && request()->segment(3) == '' ) active @endif">
-                                <a href="{{ route('permission.index') }}"><i class="fa fa-wrench"></i>
-                                    Permissions </a>
-                            </li>
-                            <li class="@if(request()->segment(2) == 'option' && request()->segment(3) == '' ) active @endif">
-                                <a href="{{ route('option.index') }}"><i class="fa fa-cog"></i> Settings </a>
-                            </li>
-                        </ul>
-                    </li>
-                    @endrole
+                <li class="nav-item menu-open">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-cogs"></i>
+                        <p>Manage <i class="right fas fa-angle-left"></i></p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="{{ route('currency.index') }}" class="nav-link">
+                                <i class="fa fa-dollar-sign nav-icon"></i>
+                                <p>Currencies</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('role.index') }}" class="nav-link">
+                                <i class="fa fa-tags nav-icon"></i>
+                                <p>Roles</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('user.index') }}" class="nav-link">
+                                <i class="fa fa-user-cog nav-icon"></i>
+                                <p>Users</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('option.index') }}" class="nav-link">
+                                <i class="fa fa-cog nav-icon"></i>
+                                <p>Options</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
             </ul>
         </nav>
         <!-- /.sidebar-menu -->
